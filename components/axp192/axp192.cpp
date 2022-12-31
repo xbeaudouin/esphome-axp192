25fcd23de93b12462e9a61a9342887fef79ab122
paulchilton <github@pchilton.co.uk>
2022-12-31 20:48:08 +0000

    Removed log entry

--- components/axp192/axp192.cpp
+++ components/axp192/axp192.cpp
@@ -205,7 +205,6 @@ void AXP192Component::ReadBuff( uint8_t Addr , uint8_t Size , uint8_t *Buff )
 
 void AXP192Component::UpdateBrightness()
 {
-    ESP_LOGD(TAG, GetStartupReason());
     ESP_LOGD(TAG, "Brightness=%f (Curr: %f)", brightness_, curr_brightness_);
     if (brightness_ == curr_brightness_)
     {
